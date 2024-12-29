157945d46f4e34f986316f56cd10d2169a087aff
hell1982bb <kloi1982bb@gmail.com>
2024-12-29 14:33:23 +0300

    Add files via upload

---
+++ config.js
@@ -0,0 +1,29 @@
+module.exports = {
+  "bot": {
+    "owners": [""],
+    "botID": "",
+    "GuildId": "",
+    "ClientId": "",
+    "serverinvte": "https://discord.gg/code",
+    "clientSECRET": "",
+    "callbackURL": "http://Adress Your Host/login",
+    "inviteBotUrl": "Link_InviteBot",
+    "TheLinkVerfy": "Link_Verfy",
+    "prefix": "-",
+    "ceatogry": "",
+    "TOKEN": "",
+    "TraId": "",
+    "price": "",
+    "channelId": "",
+    "roleId": "",
+    "LineIce": "",
+    "taxchannels": "",
+    "coinprice": ""
+  },
+  "Log": {
+    "LogChannelOwners": ""
+  },
+  "website": {
+    "PORT": "22069"
+  }
+};
